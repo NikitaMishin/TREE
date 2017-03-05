cd5ceae3f51ec0c8bc488f561fba5000f02dda6d
NikitaMishin <nikitamishin97@mail.com>
2017-03-05 23:49:38 +0300

    Completed RbIterator  with inorder traversal

--- src/RbIterator.kt
+++ src/RbIterator.kt
@@ -1,17 +1,29 @@
+import java.util.*
+
 /**
  * Created by nikita on 03.03.17.
  */
 open class RbIterator <T:Comparable<T>,P> (var node: Node<T,P>):Iterator<Node<T,P>> {
-    var queue:MyQueue<Node<T,P>>? = MyQueue()
+    //var queue:MyQueue<Node<T,P>>? = MyQueue()
+        var stack: Stack<Node<T,P>>? = Stack()
+        var queue:MyQueue<Node<T,P>> = MyQueue()
     init {
-        queue!!.add(node)
+        var tmp:Node<T,P>? = node
+        while (tmp != null) {
+            stack!!.push(tmp)
+            tmp = tmp.leftChild
+        }
+        while( stack!!.size > 0 ) {
+            tmp = stack!!.pop()
+            queue.add(tmp)
+            tmp = tmp.rightChild
+            while (tmp != null) {
+                stack!!.push( tmp)
+                tmp = tmp.leftChild
+            }
+        }
     }
-    override fun hasNext(): Boolean = queue!!.isNotEmpty()
-
-    override fun next(): Node<T,P> {
-        var tmp:Node<T,P> = queue!!.remove()
-        if(tmp.leftChild != null) queue!!.add(tmp.leftChild!!)
-        if(tmp.rightChild != null) queue!!.add(tmp.rightChild!!)
-        return tmp
-    }
+  //  //override fun hasNext(): Boolean = queue!!.isNotEmpty()
+    override  fun hasNext(): Boolean = queue.isNotEmpty()
+    override fun next(): Node<T,P> =queue.remove()
 }
