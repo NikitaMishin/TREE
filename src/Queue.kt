321ee8c32d2f63f8fca12c344f0d2e9d6917881b
NikitaMishin <nikitamishin97@mail.com>
2017-03-05 12:47:13 +0300

    Added class queue and RbIterator

---
+++ src/Queue.kt
@@ -0,0 +1,41 @@
+import java.util.Queue
+
+/**
+ * Created by nikita on 05.03.17.
+ */
+
+/**
+ * standard Queue for type P
+ * fun isEmpty return true if queue empty
+ * fun add insert element in queue
+ * fun "remove"   remove and return this element
+ */
+
+class Queue <P>()  {
+    private var head :Item<P>? = null
+    private var tail :Item<P>? = null
+    class  Item<P>( val value:P, var next :Item<P>? = null)
+
+    fun add(element: P): Boolean {
+        val item : Item<P> = Item(value = element)
+        if (tail==null){
+            head = item
+            tail = head
+        }
+        else{
+            tail?.next = item
+            tail = item
+        }
+        return true
+    }
+
+    fun remove(): P {
+        if (head == null) throw UnsupportedOperationException("Queue is empty!!")
+        val item: Item<P> = head!!
+        head = head!!.next
+        if (head ==null) tail = null
+        return item.value
+    }
+
+    fun isEmpty(): Boolean = head == null
+}
---
+++ src/RbIterator.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by nikita on 03.03.17.
+ */
+class RbIterator <T:Comparable<T>,P> (val node: Node<T,P>):Iterator<Node<T,P>> {
+    override fun hasNext(): Boolean {
+
+
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun next(): Node<T,P> {
+
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
