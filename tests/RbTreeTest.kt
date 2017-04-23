f924fab80e0f0dfee697633401c8ad202cfe8442
NikitaMishin <nikitamishin97@mail.com>
2017-04-23 13:19:46 +0300

    Added another tests

--- tests/RbTreeTest.kt
+++ tests/RbTreeTest.kt
@@ -1,49 +1,67 @@
-
+import InterfacesAndEnums.Color
 import RbTree.RbNode
 import jdk.nashorn.internal.ir.annotations.Immutable
 import org.junit.jupiter.api.Test
-
+import RbTree.RbTree
 import org.junit.jupiter.api.Assertions.*
 import java.util.*
+import RbTree.TreeIterator
 
 /**
  * Created by nikita on 13.03.17.
  */
 internal class RbTreeTest {
 
+    fun fillIdealTree(tree: RbTree<Int, Int>) {
+        tree.root = RbNode(13, 13, color = Color.BLACK)
+        tree.root!!.rightChild = RbNode(15, 15)
+        tree.root!!.rightChild!!.parent = tree.root
+        tree.root!!.leftChild = RbNode(9, 9, color = Color.RED)
+        tree.root!!.leftChild!!.parent = tree.root
+        tree.root!!.leftChild!!.rightChild = RbNode(11, 11, color = Color.BLACK)
+        tree.root!!.leftChild!!.rightChild!!.parent = tree.root!!.leftChild
+        tree.root!!.leftChild!!.leftChild = RbNode(5, 5, color = Color.BLACK)
+        tree.root!!.leftChild!!.leftChild!!.parent = tree.root!!.leftChild
+        tree.root!!.leftChild!!.leftChild!!.rightChild = RbNode(7, 7, color = Color.RED)
+        tree.root!!.leftChild!!.leftChild!!.rightChild!!.parent = tree.root!!.leftChild!!.leftChild
+        tree.root!!.leftChild!!.leftChild!!.leftChild = RbNode(3, 3, color = Color.RED)
+        tree.root!!.leftChild!!.leftChild!!.leftChild!!.parent = tree.root!!.leftChild!!.leftChild
+    }
+
+
     @Test
     fun searchInNotEmptyTreeExistedValue() {
-        val notEmptyTree = RbTree.RbTree<Int, Int>()
+        val notEmptyTree = RbTree<Int, Int>()
         for (i in 1..30 step 4) notEmptyTree.insert(i, i)
         assertEquals(5, notEmptyTree.search(5))
         assertEquals(13, notEmptyTree.search(13))
 
-    }//ok
+    }
 
     @Test
     fun searchInNotEmptyTreeUnExistedValue() {
-        var emptyTree = RbTree.RbTree<Int, Int>()
-        val notEmptyTree = RbTree.RbTree<Int, Int>()
+        var emptyTree = RbTree<Int, Int>()
+        val notEmptyTree = RbTree<Int, Int>()
         for (i in 1..30 step 7) notEmptyTree.insert(i, i)
         assertEquals(null, notEmptyTree.search(-5))
         assertEquals(null, notEmptyTree.search(-13))
         assertEquals(null, emptyTree.search(-6))
 
-    }//ok
+    }
 
     @Test
     fun searchInEmptyTree() {
-        var emptyTree = RbTree.RbTree<Int, Int>()
+        var emptyTree = RbTree<Int, Int>()
         assertEquals(null, emptyTree.search(3))
-    }//ok
+    }
 
     @Test
     operator fun iterator() {
-        var Tree: RbTree.RbTree<Int, String> = RbTree.RbTree()
+        var Tree: RbTree<Int, String> = RbTree()
         var random = Random()
         for (i in 1..1000)
             Tree.insert(random.nextInt(), random.nextFloat().toString())
-        var prev: RbTree.RbTree<Int, String>
+        var prev: RbTree<Int, String>
         val list = mutableListOf<RbNode<Int, String>>()
         for (i in Tree) list.add(i)
         for (i in 1..list.size - 2) assertEquals(true, list[i + 1].key > list[i].key)
@@ -51,35 +69,33 @@ internal class RbTreeTest {
         for (i in 1..1000000) Tree.insert(random.nextInt(), random.nextFloat().toString())
         for (i in Tree) list.add(i)
         for (i in 1..list.size - 2) assertEquals(true, list[i + 1].key > list[i].key)
-    }//ok
+    }
+
+
+    @Test
+    fun isItRbTree() {
+        val Tree = RbTree<Int, Int>()
+        fillIdealTree(Tree)
+        assertEquals(true, Tree.isItRbTree())
+    }
 
     @Test
     fun deleteInEmptyTree() {
-        val emptyTree = RbTree.RbTree<Int, Int>()
+        val emptyTree = RbTree<Int, Int>()
         assertEquals(false, emptyTree.delete(3))
-    }//ok
+    }
 
     @Test
     fun deleteRedLeaf() {
-        val notEmptyTree = RbTree.RbTree<Int, Int>()
+        val notEmptyTree = RbTree<Int, Int>()
         for (i in 1..30 step 4) notEmptyTree.insert(i, i)
-        assertEquals(true,notEmptyTree.delete(29))
-        assertEquals(true,notEmptyTree.isItRbTree())
-    }//ok
-
-    @Test
-    fun deleteNodeWithOneNode() {
-        val notEmptyTree = RbTree.RbTree<Int, Int>(RbNode(1,1))
-        assertEquals(true,notEmptyTree.delete(1))
-        assertEquals(null,notEmptyTree.root)
-    }//ok
-
-
-
+        assertEquals(true, notEmptyTree.delete(29))
+        assertEquals(true, notEmptyTree.isItRbTree())
+    }
 
     @Test
     fun insertNodeInEmptyTree() {
-        val Tree: RbTree.RbTree<Int, Int> = RbTree.RbTree()
+        val Tree: RbTree<Int, Int> = RbTree()
         Tree.insert(1, 1)
         assertEquals(1, Tree.search(1))
         Tree.insert(2, 2)
@@ -88,40 +104,74 @@ internal class RbTreeTest {
         assertEquals(null, Tree.search(4))
         Tree.insert(-5, -5)
         assertEquals(-5, Tree.search(-5))
-    }//ok
-
+    }
 
     @Test
-    fun insertNodeInTreeCase1() {//case1
-       // assertEquals(false, IdealTree.insert(5, 5))
-        var Tree = RbTree.RbTree<Int, Int>()
-        for (i in 1..100) {
-            Tree.insert(i, i)
-            assertTrue(Tree.isItRbTree())
+    fun deleteNodeWithOneNode() {
+        val notEmptyTree = RbTree<Int, Int>(RbNode(1, 1))
+        assertEquals(true, notEmptyTree.delete(1))
+        assertEquals(null, notEmptyTree.root)
+    }
+
+    @Test
+    fun delete() {
+        val Tree: RbTree<Int, Int> = RbTree()
+        for (i in 1..10000) Tree.insert(i, i)
+        for (j in 10000 downTo 1 step 3) {
+            Tree.delete(j)
+            assertEquals(true, Tree.isItRbTree())
         }
     }
 
     @Test
-    fun insertNodeInTreeCase2() {//case2
+    fun insertExistedKey(){
+        val Tree: RbTree<Int, Int> = RbTree()
+        assertEquals(true,Tree.insert(1,1))
+        assertEquals(false,Tree.insert(1,1))
 
     }
 
     @Test
-    fun insertNodeInTreeCase3() {//case3
+    fun deleteAlreadyRemovedKey(){
+        val Tree = RbTree<Int, Int>()
+        fillIdealTree(Tree)
+        assertEquals(true,Tree.delete(5))
+        assertEquals(false,Tree.delete(5))
+    }
+    @Test
+    fun deleteWhenBrotherOfRemovedChildIsBlackAndBothHisChildIsBlack() {
+        val Tree = RbTree<Int, Int>()
+        fillIdealTree(Tree)
+        val expectedTree = RbTree<Int, Int>()
+        fillIdealTree(expectedTree)
+        expectedTree.root!!.leftChild!!.leftChild!!.key = 7
+        expectedTree.root!!.leftChild!!.leftChild!!.value = 7
+        expectedTree.root!!.leftChild!!.leftChild!!.leftChild!!.parent = expectedTree.root!!.leftChild!!.leftChild
+        expectedTree.root!!.leftChild!!.leftChild!!.rightChild = null
+        val printer = TreePrinter()
+        Tree.delete(5)
+        val iterator = TreeIterator<Int, Int>(Tree.root)
+        for (i in expectedTree) if (iterator.hasNext()) assertTrue(i.equals(iterator.next()))
+    }
 
+
+    @Test
+    fun insert() {
+        val Tree: RbTree<Int, Int> = RbTree()
+        for (i in 1..10000 step 12) {
+            Tree.insert(i, i)
+            assertEquals(true, Tree.isItRbTree())
+        }
+        for (i in 10000 downTo 5 step 7) {
+            Tree.insert(i, i)
+            assertEquals(true, Tree.isItRbTree())
+        }
     }
 
     @Test
-    fun insertNodeInTreeCase4() {//case4
-    }
-
-    @Test
-    fun insertNodeInTreeCase5() {//case5
-    }
-
-    @Test
-    fun insertNodeInTreeCase6() {//case6
-
+    fun stressTest() {
+        val Tree = RbTree<Int, Int>()
+        for (i in 1..14000000 step 2) Tree.insert(i, i)
     }
 
 }
